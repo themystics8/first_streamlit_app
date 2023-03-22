9e686ff858b608fdf6dc7e8755ba596c702e1889
themystics8 <128543998+themystics8@users.noreply.github.com>
2023-03-22 23:49:20 +0530

    Update streamlit_app.py
    display value changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_to_show)
