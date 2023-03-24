4714ce373ce0f32804241c3b4d6e2fa056b4f299
themystics8 <128543998+themystics8@users.noreply.github.com>
2023-03-24 23:49:59 +0530

    Update streamlit_app.py
    error resolution

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")#watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write data in normalized form
