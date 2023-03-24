37e904e9bcd0dc0cbf0892be522922ec8d7c5d08
themystics8 <128543998+themystics8@users.noreply.github.com>
2023-03-25 00:52:47 +0530

    Update streamlit_app.py
    changing insert stmt

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('test')")
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
