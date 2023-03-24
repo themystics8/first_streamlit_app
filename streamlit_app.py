fa8010386b446893cbdbb7bd95eef1f90fe9e552
themystics8 <128543998+themystics8@users.noreply.github.com>
2023-03-25 01:03:26 +0530

    Update streamlit_app.py
    adding try block

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display api responses
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit choice:
+    streamlit.error("Please select a fruit to get information.")
+  else
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
 
 # write data in normalized form
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output data as table
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e;
+  streamlit.error()
 
 streamlit.stop()
 
