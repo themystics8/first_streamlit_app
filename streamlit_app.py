98fabbc390d48686bce06b418e72bb57b7913da5
themystics8 <128543998+themystics8@users.noreply.github.com>
2023-03-24 23:48:16 +0530

    Update streamlit_app.py
    added section for fruit choice in api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display api responses
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 
 
 import requests
